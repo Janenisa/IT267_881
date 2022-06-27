568f4f37d03d51f4e74721b48acd761ef0cf7bb0
Janenisa <644708020010@dpu.ac.th>
2022-06-27 22:32:46 +0700

    ืืn

--- modulepackage/practice_package.py
+++ modulepackage/practice_package.py
@@ -1,6 +1,6 @@
-from cusorder import customer,order #รียกแพคเกจกับโมดูล
+from cusorder import customer,order #เรียกแพคเกจกับไฟล์
 
 cus = customer.Customer("Jame", "Nonthaburi")
 ord = order.Order("15-06-2022","packed")
 
-print(f'Order of {cus.cus_name} on {ord.date} is {ord.status}')
+print(f'Order of {cus.cus_name} on {ord.date} is {ord.status}') #เรียกใช้ ฟังค์ชัน cus_name ใน Customer เรียกใช้ ฟังค์ชัน date กับ status ใน Order
